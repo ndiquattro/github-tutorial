add18ed91f2c47cf92adac667e33f3fb04c48ec6
Nick DiQuattro <ndiquattro@gmail.com>
2016-09-15 13:03:43 -0700

    moved exercise code to proper folder

---
+++ exercise1/exercise1.py
@@ -0,0 +1,9 @@
+def print_message():
+    print "NLP is confusing"
+
+def print_art():
+    print ":) :)"
+
+def hi_fellow(msg):
+    if msg == 'I am an Insight Fellow!":
+        print "That's awesome! How is that random forest model going?"
