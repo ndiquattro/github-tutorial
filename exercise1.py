9c8ec0f830cdea0efcc2dfaa45ef0f5f744bcc87
Nick DiQuattro <ndiquattro@gmail.com>
2016-09-15 12:57:35 -0700

    added hi fellow function

--- exercise1.py
+++ exercise1.py
@@ -3,3 +3,7 @@ def print_message():
 
 def print_art():
     print ":) :)"
+
+def hi_fellow(msg):
+    if msg == 'I am an Insight Fellow!":
+        print "That's awesome! How is that random forest model going?"
